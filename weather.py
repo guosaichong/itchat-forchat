7a31da488caf57417024bc6323b06b984174d6c0
guosaichong <510763683@qq.com>
2020-08-15 20:03:52 +0800

    f

--- weather.py
+++ weather.py
@@ -10,8 +10,9 @@ def weather_info():
     try:
         tianqi_url = 'http://tianqi.2345.com/ninghe1d/60517.htm'
         tianqi_response = requests.get(tianqi_url, headers=headers)
-        tianqi_response.encoding = tianqi_response.apparent_encoding
-        # print(tianqi_response.text)
+        # tianqi_response.encoding = tianqi_response.apparent_encoding
+        tianqi_response.encoding = 'utf-8'
+        print(tianqi_response.text)
         tianqi_html = etree.HTML(tianqi_response.text)
         tianqi_data1=tianqi_html.xpath('//div[@class="real-today"]/span/text()')[0][3:]
         # print(tianqi_data1)
